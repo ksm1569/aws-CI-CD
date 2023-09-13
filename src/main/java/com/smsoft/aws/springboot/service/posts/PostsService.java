02c37c68ccd9fb602ea5bcc5c26974895576c853
kimsumin <tnals1564@naver.com>
2023-09-13 17:35:26 +0900

    posts 등록과 테스트

---
+++ src/main/java/com/smsoft/aws/springboot/service/posts/PostsService.java
@@ -0,0 +1,18 @@
+package com.smsoft.aws.springboot.service.posts;
+
+import com.smsoft.aws.springboot.domain.posts.PostsRepository;
+import com.smsoft.aws.springboot.web.dto.PostsSaveRequestDto;
+import lombok.RequiredArgsConstructor;
+import org.springframework.stereotype.Service;
+import org.springframework.transaction.annotation.Transactional;
+
+@RequiredArgsConstructor
+@Service
+public class PostsService {
+    private final PostsRepository postsRepository;
+
+    @Transactional
+    public Long save(PostsSaveRequestDto requestDto) {
+        return postsRepository.save(requestDto.toEntity()).getId();
+    }
+}
---
+++ src/main/java/com/smsoft/aws/springboot/web/PostsApiController.java
@@ -0,0 +1,20 @@
+package com.smsoft.aws.springboot.web;
+
+import com.smsoft.aws.springboot.service.posts.PostsService;
+import com.smsoft.aws.springboot.web.dto.PostsSaveRequestDto;
+import lombok.RequiredArgsConstructor;
+import org.springframework.web.bind.annotation.PostMapping;
+import org.springframework.web.bind.annotation.RequestBody;
+import org.springframework.web.bind.annotation.RestController;
+
+@RequiredArgsConstructor
+@RestController
+public class PostsApiController {
+    private final PostsService postsService;
+
+    @PostMapping("/api/v1/posts")
+    public Long save(@RequestBody PostsSaveRequestDto requestDto) {
+        return postsService.save(requestDto);
+    }
+
+}
