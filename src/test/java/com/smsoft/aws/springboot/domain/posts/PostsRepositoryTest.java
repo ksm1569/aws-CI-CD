d44f2538875ca9aaaf9cc8b8d42bc1843cc8dafd
kimsumin <tnals1564@naver.com>
2023-09-14 15:53:09 +0900

    JPA Auditing 적용 및 테스트

--- src/test/java/com/smsoft/aws/springboot/domain/posts/PostsRepositoryTest.java
+++ src/test/java/com/smsoft/aws/springboot/domain/posts/PostsRepositoryTest.java
@@ -7,6 +7,7 @@ import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.boot.test.context.SpringBootTest;
 import org.springframework.test.context.junit4.SpringRunner;
 
+import java.time.LocalDateTime;
 import java.util.List;
 
 import static org.assertj.core.api.Assertions.assertThat;
@@ -42,6 +43,25 @@ public class PostsRepositoryTest {
         Posts posts = postsList.get(0);
         assertThat(posts.getTitle()).isEqualTo(title);
         assertThat(posts.getContent()).isEqualTo(content);
+    }
+
+    @Test
+    public void baseTimeEntity_test() {
+        //given
+        LocalDateTime now = LocalDateTime.of(2023,9,14,0,0,0);
+        postsRepository.save(Posts.builder()
+                        .title("this is java")
+                        .content("this is java content")
+                        .author("sumin kim")
+                        .build());
+        //when
+        List<Posts> postsList = postsRepository.findAll();
+
+        //then
+        System.out.println(">> createDate = " + postsList.get(0).getCreateDate() + ", modifiedDate = " + postsList.get(0).getModifiedDate());
+        assertThat(postsList.get(0).getCreateDate()).isAfter(now);
+        assertThat(postsList.get(0).getModifiedDate()).isAfter(now);
+
     }
 
 }
