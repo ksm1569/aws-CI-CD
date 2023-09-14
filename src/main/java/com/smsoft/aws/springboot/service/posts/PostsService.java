ab5407482b2f89438c48fbaf0b433e0421ca5d69
kimsumin <tnals1564@naver.com>
2023-09-14 14:23:06 +0900

    post 업데이트 기능 추가 및 테스트

--- src/main/java/com/smsoft/aws/springboot/service/posts/PostsService.java
+++ src/main/java/com/smsoft/aws/springboot/service/posts/PostsService.java
@@ -1,7 +1,10 @@
 package com.smsoft.aws.springboot.service.posts;
 
+import com.smsoft.aws.springboot.domain.posts.Posts;
 import com.smsoft.aws.springboot.domain.posts.PostsRepository;
+import com.smsoft.aws.springboot.web.dto.PostsResponseDto;
 import com.smsoft.aws.springboot.web.dto.PostsSaveRequestDto;
+import com.smsoft.aws.springboot.web.dto.PostsUpdateRequestDto;
 import lombok.RequiredArgsConstructor;
 import org.springframework.stereotype.Service;
 import org.springframework.transaction.annotation.Transactional;
@@ -14,5 +17,20 @@ public class PostsService {
     @Transactional
     public Long save(PostsSaveRequestDto requestDto) {
         return postsRepository.save(requestDto.toEntity()).getId();
+    }
+
+    @Transactional
+    public Long update(Long id, PostsUpdateRequestDto requestDto) {
+        Posts posts = postsRepository.findById(id).orElseThrow(() -> new IllegalArgumentException("not post. id = " + id));
+
+        posts.update(requestDto.getTitle(), requestDto.getContent());
+
+        return id;
+    }
+
+    public PostsResponseDto findById(Long id) {
+        Posts entity = postsRepository.findById(id).orElseThrow(() -> new IllegalArgumentException("not post id = " + id));
+
+        return new PostsResponseDto(entity);
     }
 }
--- src/test/java/com/smsoft/aws/springboot/web/PostsApiControllerTest.java
+++ src/test/java/com/smsoft/aws/springboot/web/PostsApiControllerTest.java
@@ -3,6 +3,7 @@ package com.smsoft.aws.springboot.web;
 import com.smsoft.aws.springboot.domain.posts.Posts;
 import com.smsoft.aws.springboot.domain.posts.PostsRepository;
 import com.smsoft.aws.springboot.web.dto.PostsSaveRequestDto;
+import com.smsoft.aws.springboot.web.dto.PostsUpdateRequestDto;
 import org.junit.After;
 import org.junit.Test;
 import org.junit.runner.RunWith;
@@ -10,6 +11,8 @@ import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.boot.test.context.SpringBootTest;
 import org.springframework.boot.test.web.client.TestRestTemplate;
 import org.springframework.boot.web.server.LocalServerPort;
+import org.springframework.http.HttpEntity;
+import org.springframework.http.HttpMethod;
 import org.springframework.http.HttpStatus;
 import org.springframework.http.ResponseEntity;
 import org.springframework.test.context.junit4.SpringRunner;
@@ -58,6 +61,40 @@ public class PostsApiControllerTest {
         assertThat(all.get(0).getTitle()).isEqualTo(title);
         assertThat(all.get(0).getContent()).isEqualTo(content);
 
+    }
+
+    @Test
+    public void posts_update() {
+        //given
+        Posts savePosts = postsRepository.save(Posts.builder()
+                        .title("effective java")
+                        .content("java content")
+                        .author("sumin")
+                        .build());
+
+        Long updateId = savePosts.getId();
+        String expectedTitle = savePosts.getTitle();
+        String expectedContent = savePosts.getContent();
+
+        PostsUpdateRequestDto requestDto = PostsUpdateRequestDto.builder()
+                .title(expectedTitle)
+                .content(expectedContent)
+                .build();
+
+        String url = "http://localhost:" + port + "/api/v1/posts/" + updateId;
+
+        HttpEntity<PostsUpdateRequestDto> requestEntity = new HttpEntity<>(requestDto);
+
+        //when
+        ResponseEntity<Long> responseEntity = restTemplate.exchange(url, HttpMethod.PUT, requestEntity, Long.class);
+
+        //then
+        assertThat(responseEntity.getStatusCode()).isEqualTo(HttpStatus.OK);
+        assertThat(responseEntity.getBody()).isGreaterThan(0L);
+
+        List<Posts> all = postsRepository.findAll();
+        assertThat(all.get(0).getTitle()).isEqualTo(expectedTitle);
+        assertThat(all.get(0).getContent()).isEqualTo(expectedContent);
     }
 
 }
