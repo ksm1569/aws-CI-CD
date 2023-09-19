c19177dcf13254d816739b64036c8c2c464d01c6
kimsumin <tnals1564@naver.com>
2023-09-19 13:58:22 +0900

    구글 로그인, 로그아웃, 권한 테스트

--- src/main/java/com/smsoft/aws/springboot/web/IndexController.java
+++ src/main/java/com/smsoft/aws/springboot/web/IndexController.java
@@ -1,5 +1,6 @@
 package com.smsoft.aws.springboot.web;
 
+import com.smsoft.aws.springboot.config.auth.dto.SessionUser;
 import com.smsoft.aws.springboot.service.posts.PostsService;
 import com.smsoft.aws.springboot.web.dto.PostsResponseDto;
 import com.smsoft.aws.springboot.web.dto.PostsUpdateRequestDto;
@@ -8,18 +9,25 @@ import org.springframework.stereotype.Controller;
 import org.springframework.ui.Model;
 import org.springframework.web.bind.annotation.GetMapping;
 import org.springframework.web.bind.annotation.PathVariable;
-import org.springframework.web.bind.annotation.PutMapping;
-import org.springframework.web.bind.annotation.RequestBody;
+
+import javax.servlet.http.HttpSession;
 
 @RequiredArgsConstructor
 @Controller
 public class IndexController {
 
     private final PostsService postsService;
+    private final HttpSession httpSession;
 
     @GetMapping("/")
     public String index(Model model) {
         model.addAttribute("posts", postsService.findAllDesc());
+        SessionUser user = (SessionUser) httpSession.getAttribute("user");
+
+        if (user != null) {
+            model.addAttribute("userName", user.getName());
+        }
+
         return "index";
     }
 
